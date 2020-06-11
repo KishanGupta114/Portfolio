9b2a3cd339e94fb468dee12655a36bfdb7570c23
kishan <kkishan114@gmail.com>
2020-06-11 17:37:13 +0530

    Updates

---
+++ precache-manifest.52b31fadbe0654f080e5619ce362ecdc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e351afa860fce8c2351654df9ef2715",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0ed2dcb0b901c3d668cf",
+    "url": "/Portfolio/static/css/main.52dd7c13.chunk.css"
+  },
+  {
+    "revision": "88df974bf6f3f78ed3fa",
+    "url": "/Portfolio/static/js/2.dfccbfa0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Portfolio/static/js/2.dfccbfa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed2dcb0b901c3d668cf",
+    "url": "/Portfolio/static/js/main.06bc712b.chunk.js"
+  },
+  {
+    "revision": "30931451511a2e372f8d",
+    "url": "/Portfolio/static/js/runtime-main.8d5cc8bf.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/Portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/Portfolio/static/media/logo.2810a88b.svg"
+  }
+]);
